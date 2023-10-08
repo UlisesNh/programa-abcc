430529c412c1613922b9b67b2a681d0a329d10dc
Ulises <unh248@gmail.com>
2023-10-08 14:57:51 -0600

    procedimientos almacenados

--- src/main/java/com/coppel/abcc/controlador/ArticuloController.java
+++ src/main/java/com/coppel/abcc/controlador/ArticuloController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import com.coppel.abcc.modelo.Articulo;
 import com.coppel.abcc.repositorio.ArticuloRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articulos")
@@ -20,27 +20,56 @@ public class ArticuloController {
         this.articuloRepository = articuloRepository;
     }
 
-
-    @GetMapping("/")
-    public String listarArticulos(Model model) {
-        List<Articulo> articulos = articuloRepository.findAll();
-        model.addAttribute("articulos", articulos);
-        return "listar-articulos";
-    }
-
- 
     @GetMapping("/crear")
     public String mostrarFormularioCreacion(Model model) {
         model.addAttribute("articulo", new Articulo());
         return "formulario-articulo";
     }
 
+  
     @PostMapping("/crear")
     public String crearArticulo(@ModelAttribute Articulo articulo) {
         articuloRepository.save(articulo);
         return "redirect:/articulos/";
     }
 
-    
+ 
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEdicion(@PathVariable Long id, Model model) {
+        Optional<Articulo> optionalArticulo = articuloRepository.findById(id);
+        if (optionalArticulo.isPresent()) {
+            model.addAttribute("articulo", optionalArticulo.get());
+            return "formulario-articulo";
+        } else {
+           
+            return "redirect:/articulos/";
+        }
+    }
 
+    
+    @PostMapping("/editar/{id}")
+    public String editarArticulo(@PathVariable Long id, @ModelAttribute Articulo articulo) {
+        articulo.setId(id);
+        articuloRepository.save(articulo);
+        return "redirect:/articulos/";
+    }
+
+   
+    @GetMapping("/eliminar/{id}")
+    public String eliminarArticulo(@PathVariable Long id) {
+        articuloRepository.deleteById(id);
+        return "redirect:/articulos/";
+    }
+
+  
+    @GetMapping("/consultar/{id}")
+    public String consultarArticulo(@PathVariable Long id, Model model) {
+        Optional<Articulo> optionalArticulo = articuloRepository.findById(id);
+        if (optionalArticulo.isPresent()) {
+            model.addAttribute("articulo", optionalArticulo.get());
+            return "detalle-articulo";
+        } else {
+            return "redirect:/articulos/";
+        }
+    }
 }
