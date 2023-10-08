2a74c6e97b6928e09ec6eaf0df52cdff40e71b70
Ulises <unh248@gmail.com>
2023-10-08 10:26:40 -0600

    second commit

---
+++ src/main/java/com/coppel/abcc/AbccCoppelApplication.java
@@ -0,0 +1,13 @@
+package com.coppel.abcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbccCoppelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbccCoppelApplication.class, args);
+	}
+
+}
